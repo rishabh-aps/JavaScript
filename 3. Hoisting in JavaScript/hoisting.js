e1de1fdd81bdccc7493ad4c09fa0fa5a2773f96e
Rishabh Shukla <rishabh.aps.8@gmail.com>
2024-06-21 19:27:51 +0530

    Day 1 Code

---
+++ 3. Hoisting in JavaScript/hoisting.js
@@ -0,0 +1,18 @@
+console.log(x);
+console.log(getName);
+console.log(getName2);
+console.log(getName3);
+
+var x = 7;
+
+var getName = () => {
+  console.log("Namaste JavaScript");
+};
+
+var getName2 = function () {
+  console.log("GetName 2");
+};
+
+function getName3() {
+  console.log("GetName3");
+}
